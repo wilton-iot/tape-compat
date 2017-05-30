8923176927c793af63c38af47772daebee587ce3
alex <alex@staticlibs.net>
2017-05-30 23:14:13 +0100

    initial commit

---
+++ index.js
@@ -0,0 +1,100 @@
+/* 
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+// assorted test-related globals required by "runNodeTests"
+define(["assert"], function(assert) {
+    "use strict";
+      
+    function test(label, func, func2) {
+        if ("function" === typeof (label)) {
+            func = label;
+            label = "unnamed";
+        }
+        if ("function" !== typeof (func) && "function" === typeof (func2)) {
+            func = func2;
+        }
+
+        print("test: " + label);
+        assert.end = function() {};
+        assert.plan = function() {};
+        assert.done = function() {};
+        assert.same = assert.deepEqual;
+        assert.notOk = function(expr, msg) {
+            return assert.ok(!expr, msg);
+        };
+        assert.test = test;
+        assert.pass = function() {};
+        assert.equals = assert.equal;
+        func(assert);
+    }
+
+    test.asyncTest = test;
+    test.describe = test;
+    test.it = test;
+    test.after = test;
+
+    test.suite = function(label) {
+        print("test: " + label);
+    };
+
+    test.QUnit = {
+        module: function(label) {
+            print("test: " + label);
+        },
+        config: {}
+    };
+
+    test.expect = function(actual) {
+        var res = {
+            to: {
+                equal: function(expected) {
+                    assert.equal(actual, expected);
+                },
+                eql: function(expected) {
+                    assert.deepEqual(actual, expected);
+                },
+                have: {
+                    length: function(expectedlen) {
+                        assert.equal(actual.length, expectedlen);
+                    }
+                },
+                not: {
+                    be: {
+                        ok: function() {
+                            assert.ok(!actual);
+                        },
+                        empty: function() {
+                            assert.ok(actual.length > 0);
+                        }
+                    }
+                },
+                be: function(expected) {
+                    assert.strictEqual(actual, expected);
+                }
+            }
+        };
+
+        res.to.be.ok = function() {
+            assert.ok(actual);
+        };
+
+        res.to.be.a = function(ctor) {
+            if ("string" === typeof (ctor)) {
+                assert.ok(ctor === typeof (actual));
+            } else {
+                assert.ok(actual instanceof ctor);
+            }
+        };
+
+        res.to.be.empty = function() {
+            assert.equal(actual.length, 0);
+        };
+
+        return res;
+    };
+    
+    return test;
+});
